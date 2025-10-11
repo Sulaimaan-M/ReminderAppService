1a7a849e294b651cf65153fb7a3b6d03d5f8046e
Mohamed Sulaimaan Nazeer Mohammad <mohamed@Mohamads-MacBook-Air.local>
2025-10-11 18:21:00 +0530

    Initial Commit

---
+++ src/main/java/com/sulaimaan/ReminderApp/ReminderAppApplication.java
@@ -0,0 +1,13 @@
+package com.sulaimaan.ReminderApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderAppApplication.class, args);
+	}
+
+}
